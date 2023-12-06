d48185b3c889bb8421e951a96bec99b69b317327
EC2 Default User <ec2-user@ip-10-0-0-64.ap-northeast-1.compute.internal>
2023-12-06 03:36:29 +0000

    post_delete

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -12,9 +12,45 @@ class Api::V1::PostsController < ApplicationController
     if post.save
       render json: post, status: :created, serializer: PostSerializer, meta: { token: token }
     else
-      render json: { status: 400, error: "posts not update" }
+      render json: { status: 400, error: "posts not create" }
     end
   end
+  
+  def update
+    jwt_authenticate
+    return render json: { status: :unauthorized, error: "Unauthorized" }, status: :unauthorized if @current_user.nil?
+    
+    token = encode(@current_user.id)
+    post = Post.find_by(id: params[:id])
+  
+    if post.nil?
+      render json: { status: :not_found, error: "Post not found" }, status: :not_found
+      return
+    end
+    
+    if post.update(post_params) # Assuming `post_params` is a method that whitelists your post parameters.
+      render json: post, status: :ok, serializer: PostSerializer, meta: { token: token }
+    else
+      render json: { status: :bad_request, error: "Post could not be updated" }, status: :bad_request
+    end
+  end
+  def destroy # Usually, the method for deleting resources in Rails is named `destroy`, not `delete`.
+    jwt_authenticate
+    return render json: { status: :unauthorized, error: "Unauthorized" }, status: :unauthorized if @current_user.nil?
+  
+    post = Post.find_by(id: params[:id])
+    
+    # It might be worth checking whether the @current_user has permissions to delete the post.
+    if post.present? && post.destroy
+      # When you are serializing one record, you have to wrap it using a key that matches the serializer
+      render json: { post: PostSerializer.new(post), meta: { token: encode(@current_user.id) } }, status: :ok
+    elsif post.nil?
+      render json: { status: :not_found, error: "Post not found" }, status: :not_found
+    else
+      render json: { status: :bad_request, error: "Post could not be deleted" }, status: :bad_request
+    end
+  end
+  
   
   private
   def post_params
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,17 +1,54 @@
 class Api::V1::UsersController < ApplicationController
-    skip_before_action :jwt_authenticate, only: [:create]
-    def create
-        user=User.new(user_params)
-        if user.save
-        token = encode(user.id)
-        render json:{status:200,data: {name: user.name, email: user.email, token: token}}
-        else
-        render json: {status:400,error: "users can't save and create"}
-        end
+  skip_before_action :jwt_authenticate, only: [:create]
+  
+  def create
+    user = User.new(user_params)
+    if user.save
+      token = encode(user.id)
+      render json: {status: 200, data: {name: user.name, email: user.email, token: token}}
+    else
+      render json: {status: 400, error: "User cannot be created"}
     end
-    private
-    def user_params
-      params.require(:user).permit(:name,:email,:password,:password_confirmation)
-    end
+  end 
 
+  def show
+    jwt_authenticate
+    return render json: { status: :unauthorized, error: "Unauthorized" }, status: :unauthorized if @current_user.nil?
+    
+    user = User.find(params[:id])
+    posts = user.posts.all
+    
+    serialized_user = UserSerializer.new(user).as_json
+    token = encode(user.id)
+    
+    render json: {
+      status: 200,
+      data: {
+        user: serialized_user,
+        token: token,
+        posts: posts.as_json(only: [:id, :title, :content])
+      }
+    }
+  rescue ActiveRecord::RecordNotFound
+    render json: { status: 404, error: "User not found" }
+  end
+
+  def update
+    jwt_authenticate
+    return render json: { status: :unauthorized, error: "Unauthorized" }, status: :unauthorized if @current_user.nil?
+    
+    user = User.find(params[:id])
+    token = encode(user.id)
+    
+    if user.update(user_params)
+      render json: {status: 200, data: {name: user.name, email: user.email, token: token}}
+    else
+      render json: {status: 400, error: "User cannot be updated"}
+    end
+  end
+  
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
